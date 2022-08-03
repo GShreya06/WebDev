209051cb380bdd1ad43df3ca913069f35a4a4c47
GShreya06 <shreyagupta4563@gmail.com>
2022-08-03 22:39:20 +0530

    Added file

--- js/file1.js
+++ js/file1.js
@@ -1,1 +1,20 @@
-console.log("Hello world")
+// console.log("Hello world")
+// var a=45;
+// var b="Hello"
+// console.log(a);
+
+// var num1=2;
+// var num2=5;
+
+// console.log("The value of num1 + num2 is " + (num1+num2));
+// console.log("The value of num1 - num2 is " + (num1-num2));
+// console.log("The value of num1 * num2 is " + (num1*num2));
+// console.log("The value of num1 / num2 is " + (num1/num2));
+// console.log("The value of num1 ** num2 is " + (num1**num2));
+// console.log("The value of num1++ is " + (num1++));
+// console.log("The value of ++num1 is " + (++num1));
+// console.log("The value of num2-- is " + (num2--));
+// console.log("The value of --num2 is " + (--num2));
+
+var string = "Hello";
+console.log(string);
